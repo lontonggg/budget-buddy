a2261b3d01728d607738b959fc1f5a641c83e71a
lontonggg <zadareyhan@gmail.com>
2024-03-01 21:56:27 +0700

    Improved home page design

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -3,19 +3,28 @@
 import React from 'react'
 
 export const Balance = ({user}) => {
+    const formattedIncome = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(
+        user.income,
+      );
+    const formattedExpense = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(
+    user.expense,
+    );
+    const formattedBalance= new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(
+    user.balance,
+    );
     return (
         <div className='flex bg-white gap-20 justify-center items-center px-20 py-10 w-fit rounded-xl shadow-xl'>
             <div>
                 <div className='text-xl'>Income</div>
-                <div className='font-bold text-blue-500 text-2xl'>Rp {user.income}</div>
+                <div className='font-bold text-blue-500 text-2xl'>{formattedIncome}</div>
             </div>  
             <div>
                 <div className='text-xl'>Expenses</div>
-                <div className='font-bold text-red-500 text-2xl'>Rp {user.expense}</div>
+                <div className='font-bold text-red-500 text-2xl'>{formattedExpense}</div>
             </div>
             <div>
                 <div className='text-xl'>Total Balance</div>
-                <div className='font-bold text-2xl'>Rp {user.balance}</div>
+                <div className='font-bold text-2xl'>{formattedBalance}</div>
             </div>
         </div>
   )
--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export const Transactions = ({transaction}) => {
+    const formattedAmount = new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(
+        transaction.amount,
+      );
   return (
     <div className='flex rounded-xl justify-between items-center p-5 shadow-xl border-slate-300 border-2'>
        <div className='flex flex-col gap-3'>
@@ -8,7 +11,11 @@ export const Transactions = ({transaction}) => {
             <p className='text-sm text-gray-500'>{transaction.description}</p>
        </div>
         <div>
-            <p className='text-xl font-bold'>{transaction.amount}</p>
+            {transaction.type === "Income" ? (
+                <p className='text-xl font-bold text-blue-500'>{formattedAmount}</p>
+            ): (
+                <p className='text-xl font-bold text-red-500'>{formattedAmount}</p>
+            )}
         </div>
     </div>
 
