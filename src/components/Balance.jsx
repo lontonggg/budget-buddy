144619cb9aee5a6815bc6453d56db2504fa5b98b
lontonggg <zadareyhan@gmail.com>
2024-03-02 02:33:03 +0700

    Added more interactivity

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -14,15 +14,15 @@ export const Balance = ({user}) => {
     );
     return (
         <div className='flex bg-white gap-20 justify-center items-center px-20 py-10 w-fit rounded-xl shadow-xl'>
-            <div>
+            <div className='hover:scale-105 transition-transform duration-300'>
                 <div className='text-xl'>Income</div>
                 <div className='font-bold text-blue-500 text-2xl'>{formattedIncome}</div>
             </div>  
-            <div>
+            <div className='hover:scale-105 transition-transform duration-300'>
                 <div className='text-xl'>Expenses</div>
                 <div className='font-bold text-red-500 text-2xl'>{formattedExpense}</div>
             </div>
-            <div>
+            <div className='hover:scale-105 transition-transform duration-300'>
                 <div className='text-xl'>Total Balance</div>
                 <div className='font-bold text-2xl'>{formattedBalance}</div>
             </div>
